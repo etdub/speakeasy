89473c5ec24d38b77ac2b27156ea26c08703dfcb
Eric Wong <eric.wong.t@gmail.com>
2013-10-16 23:34:15 +0000

    Remove MANIFEST file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='speakeasy',
-    version='0.0.5',
+    version='0.0.6',
     description='Metrics aggregation server',
     author='Eric Wong',
     packages=['speakeasy', 'speakeasy.emitter'],
