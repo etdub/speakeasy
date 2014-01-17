7c103bf8a638fed325087f05bc128471e8978f02
Eric Wong <eric.wong.t@gmail.com>
2014-01-17 01:55:36 +0000

    bump ver

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='speakeasy',
-    version='1.0.0',
+    version='1.0.1',
     description='Metrics aggregation server',
     author='Eric Wong',
     packages=['speakeasy', 'speakeasy.emitter'],
