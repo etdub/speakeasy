24059aab9045bc60caec38097d581277d340cd92
Qingping Hou <qihou@linkedin.com>
2014-10-01 13:28:43 -0700

    test: add test_zmq_spec

--- test/test_speakeasy.py
+++ test/test_speakeasy.py
@@ -1,17 +1,35 @@
 import unittest2 as unittest
+import random
+import socket
+import time
+import json
 import zmq
+from test_util import get_random_free_port
 from speakeasy.speakeasy import Speakeasy
 
+G_SPEAKEASY_HOST = '0.0.0.0'
+G_PUB_PORT = str(get_random_free_port())
+G_CMD_PORT = str(get_random_free_port())
+G_METRIC_SOCKET = '/var/tmp/test_metric_{0}'.format(random.random())
+G_LEGACY_METRIC_SOCKET = '/var/tmp/legacy_metric_{0}'.format(random.random())
 
 def gen_speakeasy_server():
-    return Speakeasy('0.0.0.0', '/var/tmp/test_metric_socket', '50001', '50002',
-                     'simple', ['filename=/var/tmp/test_metrics.out'], 60,
-                     '/var/tmp/test_metric_socket2')
+    return Speakeasy(G_SPEAKEASY_HOST, G_METRIC_SOCKET, G_CMD_PORT, G_PUB_PORT,
+                     'simple', ['filename=/var/tmp/test_metrics.out'],
+                     60, G_LEGACY_METRIC_SOCKET)
 
 class TestSpeakeasy(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.srv = gen_speakeasy_server()
+
+        self.sub_socket = zmq.Context().socket(zmq.SUB)
+        self.sub_socket.connect('tcp://localhost:{0}'.format(G_PUB_PORT))
+        self.sub_socket.setsockopt(zmq.SUBSCRIBE, '')
+
+        self.poller = zmq.Poller()
+        self.poller.register(self.sub_socket, zmq.POLLIN)
+
         self.srv.start()
 
     @classmethod
@@ -22,14 +40,23 @@ class TestSpeakeasy(unittest.TestCase):
         pass
 
     def tearDown(self):
-        self.srv.metrics = {}
+        with self.srv.metrics_lock:
+            self.srv.metrics = {}
+
+    def clear_sub_socket(self):
+        while True:
+            socks = dict(self.poller.poll(500))
+            if self.sub_socket in socks:
+                self.sub_socket.recv() # suck out zmq pub event
+            else:
+                break
 
     # we use _0_ to force init test run first
     def test_0_server_init(self):
-        self.assertEqual(self.srv.metric_socket, '/var/tmp/test_metric_socket')
-        self.assertEqual(self.srv.legacy, '/var/tmp/test_metric_socket2')
-        self.assertEqual(self.srv.cmd_port, '50001')
-        self.assertEqual(self.srv.pub_port, '50002')
+        self.assertEqual(self.srv.metric_socket, G_METRIC_SOCKET)
+        self.assertEqual(self.srv.legacy, G_LEGACY_METRIC_SOCKET)
+        self.assertEqual(self.srv.cmd_port, G_CMD_PORT)
+        self.assertEqual(self.srv.pub_port, G_PUB_PORT)
         self.assertEqual(self.srv.emission_interval, 60)
         self.assertEqual(self.srv.emitter_args['filename'],
                          '/var/tmp/test_metrics.out')
@@ -40,6 +67,20 @@ class TestSpeakeasy(unittest.TestCase):
         self.srv.process_metric(['test_app', 'test_metric', 'GAUGE', 1])
         metric = self.srv.metrics['test_app']['GAUGE']['test_metric']
         self.assertTrue(sorted(metric) == [1.0])
+
+    def test_legacy_socket(self):
+        self.clear_sub_socket()
+        legacy_sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+        legacy_sock.connect(G_LEGACY_METRIC_SOCKET)
+        legacy_sock.send('test_cnt3|10|COUNTER')
+        self.assertGreater(len(dict(self.poller.poll(500))), 0)
+        metrics = json.loads(self.sub_socket.recv())
+        self.assertEqual(len(metrics), 1)
+        self.assertEqual(metrics[0][0], G_SPEAKEASY_HOST)
+        self.assertEqual(metrics[0][1], u'__LEGACY__')
+        self.assertEqual(metrics[0][2], u'test_cnt3')
+        self.assertEqual(metrics[0][3], 'COUNTER')
+        self.assertEqual(metrics[0][4], 10)
 
 
 if __name__ == '__main__':
